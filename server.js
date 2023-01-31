a106b1e23a724a127447a77c3838362886e51b2a
Mesbah-Himel <mesbah0010@gmail.com>
2023-01-31 15:28:41 +0600

    new commit

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+//static files
+app.use(express.static(path.join(__dirname,"./client/build")))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname , "./client/build/index.html"))
+});
+
+
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Node Express Server Started at ${port}!`));
